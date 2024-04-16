498c199adef4c98153600b46da0fbb8e66f63c29
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-16 07:12:46 -0400

    Cours 3

--- resto/resources/views/partials/header.blade.php
+++ resto/resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="flex-none min-h-10 bg-amber-50 flex justify-center gap-10 shadow-md  z-10">
     <div class="logo ">
-        <img class="h-auto max-h-24 object-contain" src="assets/img/logo.png" alt="">
+        <img class="h-auto max-h-24 object-contain" src="#" alt="">
     </div>
     @include('partials.nav')
 </header>
