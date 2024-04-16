5998b4853086440bab2caa24e7cd99cc213c784e
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-16 08:27:58 -0400

    no date

--- resto/resources/views/partials/footer.blade.php
+++ resto/resources/views/partials/footer.blade.php
@@ -1,6 +1,3 @@
 <footer class=" flex-none min-h-10 bg-amber-950 p-3">
-    @php
-        $date = date('Y');
-    @endphp
-    <p class="text-center text-amber-100 text-sm italic">© {{ $date }} Resto à déjeuner</p>
+    <p class="text-center text-amber-100 text-sm italic">© Resto à déjeuner</p>
 </footer>
