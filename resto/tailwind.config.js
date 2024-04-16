1e97aabe3ee5fe6e24beb121d00d362118336a37
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-04-16 08:19:25 -0400

    cours 3

--- resto/tailwind.config.js
+++ resto/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                patate: "#ff0000",
+            },
+        },
     },
     plugins: [],
 };
