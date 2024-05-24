48db3ce9abac63777d7f564ac2c7bc2026176b81
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2024-05-24 15:06:27 -0400

    Modif route

--- resto/resources/views/partials/header.blade.php
+++ resto/resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
     </div>
     @include('partials.nav')
     <div class="flex justify-center items-center gap-6">
-        <a href="/locale?lang=en">EN</a>
+        <a href={{ route("locale", ["lang"=>"en"]) }}>EN</a>
         <a href="/locale?lang=fr">FR</a>
     </div>
 
